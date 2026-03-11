276baf70ad3cc51d8a6e122cb8c3510dcce2d886
krzysztof <krzysztof.wojczakowski@gmail.com>
2026-03-11 10:46:49 +0100

    polowa

--- zadania_5/zadanie_2.c
+++ zadania_5/zadanie_2.c
@@ -5,12 +5,38 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
+#include<string.h>
 
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 
 void usage(char *name) {
     fprintf(stderr, "USAGE: %s n\n", name);
     exit(EXIT_FAILURE);
+}
+
+void player(int id,int read_pipe,int write_pipe,int M){
+    char komunikat[16];
+    char newround[16] = "new_round"; // jesli to bedzie w komunikacie to bedzie oznaczalo losuj liczbe
+    char end[16] = "end_game"; // jesli to bedzie w komunikacie bedzie oznaczalo koniec gry
+    
+    while(1){
+        memset(komunikat, 0, sizeof(komunikat)); // Wypełnia całą tablicę zerami
+        int status = read(read_pipe,komunikat,sizeof(komunikat));
+        if(status < 0){
+            if(errno == EINTR) continue;
+            else ERR("read");
+        }
+        if(status == 0) break; // nasz odbiorca przestal istniec 
+        int points;
+        if(komunikat[0] == 'l'){ // jesli pierwsza literka bedzie l to bedzie nastepnie liczba
+            points = atoi(komunikat+1);
+        }else if(strcmp(komunikat,newround)==0){
+            
+        }
+    }
+
+    if(close(read_pipe)<0) ERR("close pipe");
+    if(close(write_pipe)<0) ERR("close pipe");
 }
 
 int main(int argc,char** argv){
@@ -26,5 +52,24 @@ int main(int argc,char** argv){
         if(pipe(PS[i])==-1) ERR("pipe");
         if(pipe(SP[i])==-1) ERR("pipe");
     }
-    
+    for(int i=0;i<n;i++){
+        children[i] = fork();
+        if(children[i] == -1) ERR("fork");
+        if(children[i] == 0){
+            player(i,PS[i][0],SP[i][1],m);
+            exit(EXIT_SUCCESS);
+        }
+        // rodzic :
+        if(close(PS[i][0])==-1) ERR("close"); // zamykamy czytanie od serwera
+        if(close(SP[i][1])==-1) ERR("close"); // zamykamy pisanie od dziecka
+    }
+    // rundy
+    int winners = 0;
+    int winners_id[n];
+    int points[n];
+    char komunikat[16] = "new_round";
+    for(int r=0;r<m;r++){
+
+    }
+
 }
