edde28b35bb691859eb6765ad849ff21288dd192
Krzychulec <criskbp.2019@gmail.com>
2026-03-11 13:58:30 +0100

    zadanie domowe

---
+++ zadania_5/w5/sop-roncevaux.c
@@ -0,0 +1,70 @@
+#define _POSIX_C_SOURCE 200809L
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ERR(source) \
+    (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), perror(source), kill(0, SIGKILL), exit(EXIT_FAILURE))
+
+int set_handler(void (*f)(int), int sig)
+{
+    struct sigaction act = {0};
+    act.sa_handler = f;
+    if (sigaction(sig, &act, NULL) == -1)
+        return -1;
+    return 0;
+}
+
+void msleep(int millisec)
+{
+    struct timespec tt;
+    tt.tv_sec = millisec / 1000;
+    tt.tv_nsec = (millisec % 1000) * 1000000;
+    while (nanosleep(&tt, &tt) == -1)
+    {
+    }
+}
+
+int count_descriptors()
+{
+    int count = 0;
+    DIR* dir;
+    struct dirent* entry;
+    struct stat stats;
+    if ((dir = opendir("/proc/self/fd")) == NULL)
+        ERR("opendir");
+    char path[PATH_MAX];
+    getcwd(path, PATH_MAX);
+    chdir("/proc/self/fd");
+    do
+    {
+        errno = 0;
+        if ((entry = readdir(dir)) != NULL)
+        {
+            if (lstat(entry->d_name, &stats))
+                ERR("lstat");
+            if (!S_ISDIR(stats.st_mode))
+                count++;
+        }
+    } while (entry != NULL);
+    if (chdir(path))
+        ERR("chdir");
+    if (closedir(dir))
+        ERR("closedir");
+    return count - 1;  // one descriptor for open directory
+}
+
+int main(int argc, char* argv[])
+{
+    srand(time(NULL));
+    printf("Opened descriptors: %d\n", count_descriptors());
+}
