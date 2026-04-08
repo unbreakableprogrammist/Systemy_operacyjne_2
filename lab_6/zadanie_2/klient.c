cce6954453fc980b403b020eae392fa5a56a55cd
krzysztof <krzysztof.wojczakowski@gmail.com>
2026-04-08 10:53:57 +0200

    all

--- lab_6/zadanie_2/klient.c
+++ lab_6/zadanie_2/klient.c
@@ -22,3 +22,52 @@ void usage(char* name)
     fprintf(stderr, "USAGE: %s server_pid\n", name);
     exit(EXIT_FAILURE);
 }
+
+int main(int argc,char** argv){
+    if(argc != 2) usage("number of arguments");
+
+    int server_pid = atoi(argv[1]);
+    if (server_pid == 0)
+        usage(argv[0]);
+    srand(getpid());
+
+    char file_name[30];
+    sprintf(file_name,"/%d-board",server_pid);
+
+    int shm = shm_open(file_name,O_RDWR,0666);
+
+    if(shm == -1) ERR("shm open");
+
+    char* shm_ptr = (char*)mmap(NULL,SHM_SIZE,PROT_READ | PROT_WRITE,MAP_SHARED,shm,0);
+    if(shm_ptr == MAP_FAILED) ERR("mmap");
+
+    pthread_mutex_t* mtx = (pthread_mutex_t*)shm_ptr;
+    int N = *(int*)(shm_ptr+sizeof(pthread_mutex_t));
+    char* board = shm_ptr + sizeof(pthread_mutex_t) + sizeof(int);
+
+    int points = 0;
+    while(1){
+        int error;
+        if((error = pthread_mutex_lock(mtx))!=0){
+            if(error == EOWNERDEAD) pthread_mutex_consistent(mtx);
+            else ERR("mutex");
+        }
+        int random = 1 + rand()%10;
+        if(random == 1){
+            printf("OOPS Finisz \n");
+            exit(EXIT_SUCCESS);
+        }
+        int x = rand()%N;
+        int y = rand()%N;
+        if(board[x*N+y] != 0){
+            printf("found %d points\n",(int)board[x*N+y]);
+            points+=(int)board[x*N+y];
+            board[x*N+y] = 0;
+        }else{
+            printf("GAME OVER, SCORE: %d\n", points);
+            pthread_mutex_unlock(mtx);
+            exit(EXIT_SUCCESS);
+        }
+        pthread_mutex_unlock(mtx);
+    }
+}
--- lab_6/zadanie_2/serwer.c
+++ lab_6/zadanie_2/serwer.c
@@ -23,74 +23,87 @@ void usage(char* name)
     fprintf(stderr, "3 < N <= 30 - board size\n");
     exit(EXIT_FAILURE);
 }
+
 volatile sig_atomic_t running = 1;
 void sigint_handler(int sig){
     running = 0;
 }
 
-int main(int argc,char ** argv){
-    if(argc != 2) usage(argc);
+int main(int argc, char ** argv){
+    if(argc != 2) usage(argv[0]); // Poprawione: argv[0]
     int n = atoi(argv[1]);
-    if(n<=0 || n>=30) usage(n);
+    if(n <= 0 || n >= 30) usage(argv[0]); // Poprawione: argv[0]
 
     pid_t my_pid = getpid();
     srand(my_pid); 
-    printf("MY PID IS : %d\n",my_pid);
+    printf("MY PID IS : %d\n", my_pid);
 
     char file_name[40];
-    sprintf(file_name,"/%d-board",my_pid);
-    int fd = shm_open(file_name,O_CREAT | O_EXCL | O_RDWR,0666);
-    if(fd < 0 ) ERR("shm open");
+    sprintf(file_name, "/%d-board", my_pid);
+    
+    // O_EXCL jest super - serwer nie nadpisze istniejącej planszy
+    int fd = shm_open(file_name, O_CREAT | O_EXCL | O_RDWR, 0666);
+    if(fd < 0) ERR("shm open");
 
-    if(ftruncate(fd,SHM_SIZE) == -1) ERR(ftruncate);
+    if(ftruncate(fd, SHM_SIZE) == -1) ERR("truncate");
 
-    char* shm_ptr = (char*)mmap(NULL,SHM_SIZE,PROT_READ | PROT_WRITE , MAP_SHARED , fd, 0);
-    if(shm_ptr == MAP_FAILED) ERR(mmap);
+    char* shm_ptr = (char*)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if(shm_ptr == MAP_FAILED) ERR("mmap"); // Poprawione: cudzysłów
  
-    // zapisujemy sobie poczatki wskaznikow 
+    // Podział pamięci: [Mutex][N][Board]
     pthread_mutex_t* mutex = (pthread_mutex_t*)shm_ptr;  
-    char* N_shared = shm_ptr + sizeof(pthread_mutex_t);
-    char* board_shared = N_shared + sizeof(int);
+    int* N_shared = (int*)(shm_ptr + sizeof(pthread_mutex_t));
+    char* board_shared = shm_ptr + sizeof(int) + sizeof(pthread_mutex_t);
 
+    // Konfiguracja ROBUST MUTEX
     pthread_mutexattr_t mutex_attr;
-    if(pthread_mutexattr_init(&mutex_attr) != 0) ERR("mutex attr init");
-    if(pthread_mutexattr_setpshared(&mutex_attr,PTHREAD_PROCESS_SHARED) != 0) ERR("mutex attr setpshared");
-    if(pthread_mutexattr_setrobust(&mutex_attr,PTHREAD_MUTEX_ROBUST) != 0) ERR("mutex attr setrobust");
-    if(pthread_mutex_init(mutex,&mutex_attr) != 0) ERR("mutex init");
-    if(pthread_mutexattr_destroy(&mutex_attr) != 0) ERR("mutex attr destroy"); // atrybuty juz niepotrzebne
+    pthread_mutexattr_init(&mutex_attr);
+    pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED);
+    pthread_mutexattr_setrobust(&mutex_attr, PTHREAD_MUTEX_ROBUST);
+    pthread_mutex_init(mutex, &mutex_attr);
+    pthread_mutexattr_destroy(&mutex_attr);
 
     *N_shared = n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            board_shared[i*n+j] = 1 + rand() % 9; // losujemy liczby od 1 do 9
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            board_shared[i * n + j] = 1 + rand() % 9;
         }
     }
 
     struct sigaction sa;
-    memset(&sa,0,sizeof(sa));
+    memset(&sa, 0, sizeof(sa));
     sa.sa_handler = sigint_handler;
-    if(sigaction(SIGINT,&sa,NULL) == -1) ERR("sigaction");
+    if(sigaction(SIGINT, &sa, NULL) == -1) ERR("sigaction");
 
     while(running){
         int error;
-        if(error = pthread_mutex_lock(mutex) != 0){
-            if(error == EOWNERDEAD) pthread_mutex_consistent(mutex);
-            else ERR("mutex lock");
+        // Poprawione priorytety operatorów przez nawiasy
+        if((error = pthread_mutex_lock(mutex)) != 0){
+            if(error == EOWNERDEAD) {
+                pthread_mutex_consistent(mutex);
+            } else {
+                ERR("mutex lock");
+            }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;i<n;j++){
-                printf("%d",board_shared[i*n+j]);
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){ // Poprawione: j < n
+                printf("%d ", board_shared[i * n + j]); // Dodana spacja
             }
             printf("\n");
         }
+        
         pthread_mutex_unlock(mutex);
-        struct timespec t = {3,0};
-        while(nanosleep(&t,NULL) > 0){}
+        printf("------------------------------\n");
+        struct timespec t = {3, 0};
+        nanosleep(&t, NULL);
     }
+    
+    // Sprzątanie po wyjściu z pętli
     pthread_mutex_destroy(mutex);
-    munmap(shm_ptr,1024);
+    munmap(shm_ptr, SHM_SIZE);
     shm_unlink(file_name);
 
+    printf("\nServer cleaned up and exited.\n");
     return EXIT_SUCCESS;
-
 }
