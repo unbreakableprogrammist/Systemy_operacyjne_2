8e8e9196513686af061226c54812efcabafef45f
krzysztof <krzysztof.wojczakowski@gmail.com>
2026-04-08 22:30:13 +0200

    etap 3

--- zadania_6/w6/sop-shop.c
+++ zadania_6/w6/sop-shop.c
@@ -71,76 +71,180 @@ void print_array(int* array, int n)
     printf("\n");
 }
 
-void children_work(int id,int* shelves,pthread_mutex_t* mutexes,int n){
-    printf("%d worker starts shift\n",getpid());
-    srand(time(NULL) ^ (getpid()));
-    for(int i=0;i<10;i++){
-        int shelf1 = rand() % n;
-        int shelf2;
-        do{
-            shelf2 = rand() % n;
-        }while(shelf1 == shelf2);
+// Zmiana w strukturze: lepsza nazwa i dodatkowe pola z Etapu 4
+typedef struct {
+    int running;
+    pthread_mutex_t mutex;
+    int ile; // liczba pracowników, którzy skończyli pracę
+} shared_data_t;
 
-        int low = shelf1 < shelf2 ? shelf1 : shelf2; // mniejszy indeks
-        int high = shelf1 > shelf2 ? shelf1 : shelf2;
-        
-        int error1 = pthread_mutex_lock(&mutexes[low]);
-        if(error1 == EOWNERDEAD) pthread_mutex_consistent(&mutexes[low]);
-        else if(error1 != 0) ERR("pthread_mutex_lock");
-        int error2 = pthread_mutex_lock(&mutexes[high]); 
-        if(error2 == EOWNERDEAD) pthread_mutex_consistent(&mutexes[high]);
-        else if(error2 != 0) ERR("pthread_mutex_lock");
+// Poprawione nagłówki funkcji - używamy pthread_mutex_t*
+void children_work(int id, int* shelves, pthread_mutex_t* mutexes, int n, int m) {
+    printf("[%d] Worker reports for a night shift\n", getpid()); // 
+    srand(time(NULL) ^ getpid());
+    
+    // Rzutujemy pamięć za muteksami na strukturę sterującą
+    shared_data_t* sd = (shared_data_t*)((char*)mutexes + n * sizeof(pthread_mutex_t));
 
-        if(shelves[low] > shelves[high]){
-            int pom = shelves[low];
-            shelves[low] = shelves[high];
-            shelves[high] = pom;
+    for(int i = 0; i < 10; i++) {
+        // Sprawdzenie flagi zakończenia
+        pthread_mutex_lock(&sd->mutex);
+        if(!sd->running) {
+            pthread_mutex_unlock(&sd->mutex);
+            break;
         }
+        pthread_mutex_unlock(&sd->mutex);
+
+        int s1 = rand() % n;
+        int s2;
+        do { s2 = rand() % n; } while(s1 == s2);
+
+        int low = s1 < s2 ? s1 : s2;
+        int high = s1 > s2 ? s1 : s2;
+        
+        // Prawidłowe blokowanie struktur muteksów
+        pthread_mutex_lock(&mutexes[low]);
+        pthread_mutex_lock(&mutexes[high]);
+
+        if(shelves[low] > shelves[high]) {
+            swap(&shelves[low], &shelves[high]);
+            ms_sleep(100); // [cite: 30]
+        }
+
         pthread_mutex_unlock(&mutexes[high]);
         pthread_mutex_unlock(&mutexes[low]);
-   
     }
+
+    pthread_mutex_lock(&sd->mutex);
+    sd->ile++;
+    pthread_mutex_unlock(&sd->mutex);
 }
 
-int main(int argc, char** argv) {     
-    if(argc != 3) usage(argv[0]);
+void manager_work(int* shelves, pthread_mutex_t* mutexes, int n, int m) {
+    printf("[%d] Manager reports for a night shift\n", getpid()); // 
+    shared_data_t* sd = (shared_data_t*)((char*)mutexes + n * sizeof(pthread_mutex_t));
+
+    while(1) {
+        ms_sleep(500); // 
+        print_array(shelves, n); // 
+        msync(shelves, n * sizeof(int), MS_SYNC); // 
+
+        pthread_mutex_lock(&sd->mutex);
+        
+        // Sprawdzenie czy wszyscy skończyli
+        if(sd->ile == m) {
+            sd->running = 0;
+            pthread_mutex_unlock(&sd->mutex);
+            break;
+        }
+
+        // Sprawdzenie porządku
+        int sorted = 1;
+        for(int i = 1; i < n; i++) {
+            if(shelves[i-1] > shelves[i]) {
+                sorted = 0;
+                break;
+            }
+        }
+
+        if(sorted) {
+            printf("[%d] The shop shelves are sorted\n", getpid()); // 
+            sd->running = 0;
+            pthread_mutex_unlock(&sd->mutex);
+            break;
+        }
+        pthread_mutex_unlock(&sd->mutex);
+    }
+}
+int main(int argc, char** argv) {
+    // 1. Walidacja argumentów wejściowych [cite: 23]
+    if (argc != 3) usage(argv[0]);
     int n = atoi(argv[1]);
     int m = atoi(argv[2]);
-    if(n < 8 || n > 256) usage(argv[0]);
-    if(m < 1 || m > 64) usage(argv[0]);
+    if (n < MIN_SHELVES || n > MAX_SHELVES) usage(argv[0]);
+    if (m < MIN_WORKERS || m > MAX_WORKERS) usage(argv[0]);
 
-    // create file 
+    // 2. Przygotowanie pliku SHOP_FILENAME i mapowanie półek [cite: 25]
     int fd = open(SHOP_FILENAME, O_RDWR | O_CREAT | O_TRUNC, 0600);
-    if(fd == -1) ERR("open");
-    if(ftruncate(fd,n*sizeof(int)) == -1) ERR("ftruncate");
+    if (fd == -1) ERR("open");
+    if (ftruncate(fd, n * sizeof(int)) == -1) ERR("ftruncate");
 
-    int* shm_ptr = (int*)mmap(NULL,n*sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    if(shm_ptr == MAP_FAILED) ERR("mmap");
+    int* shm_ptr = (int*)mmap(NULL, n * sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (shm_ptr == MAP_FAILED) ERR("mmap shop");
+    close(fd); // Deskryptor nie jest już potrzebny po mmap
 
-    pthread_mutex_t* mutex_ptr = (pthread_mutex_t*)mmap(NULL,n*sizeof(pthread_mutex_t),PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    if(mutex_ptr == MAP_FAILED) ERR("mmap");
+    // 3. Mapowanie anonimowej pamięci na muteksy i zmienne współdzielone [cite: 32, 33]
+    size_t shared_size = n * sizeof(pthread_mutex_t) + sizeof(shared_data_t);
+    void* shm_base = mmap(NULL, shared_size, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    if (shm_base == MAP_FAILED) ERR("mmap anonymous");
+
+    // Wyznaczamy wskaźniki wewnątrz zmapowanej pamięci
+    pthread_mutex_t* mutex_ptr = (pthread_mutex_t*)shm_base;
+    shared_data_t* sd = (shared_data_t*)((char*)shm_base + n * sizeof(pthread_mutex_t));
+
+    // 4. Konfiguracja atrybutów muteksów (Robust i Process Shared) 
     pthread_mutexattr_t mutex_attr;
-    if(pthread_mutexattr_init(&mutex_attr)) ERR("pthread_mutexattr_init");
-    if(pthread_mutexattr_setpshared(&mutex_attr,PTHREAD_PROCESS_SHARED)) ERR("pthread_mutexattr_setpshared");
-    if(pthread_mutexattr_setrobust(&mutex_attr,PTHREAD_MUTEX_ROBUST)) ERR("pthread_mutexattr_setrobust");
-    for(int i=0;i<n;i++){
-        if(pthread_mutex_init(&mutex_ptr[i],&mutex_attr)) ERR("pthread mutex init ");
+    if (pthread_mutexattr_init(&mutex_attr)) ERR("pthread_mutexattr_init");
+    if (pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED)) ERR("pthread_mutexattr_setpshared");
+    if (pthread_mutexattr_setrobust(&mutex_attr, PTHREAD_MUTEX_ROBUST)) ERR("pthread_mutexattr_setrobust");
+
+    // Inicjalizacja muteksów dla każdej półki
+    for (int i = 0; i < n; i++) {
+        if (pthread_mutex_init(&mutex_ptr[i], &mutex_attr)) ERR("pthread_mutex_init shelf");
     }
+
+    // Inicjalizacja struktury kontrolnej
+    sd->running = 1;
+    sd->ile = 0; // Licznik pracowników, którzy skończyli (Etap 3)
+    // alive_count zainicjalizujesz tutaj, gdy przejdziesz do Etapu 4
+    if (pthread_mutex_init(&sd->mutex, &mutex_attr)) ERR("pthread_mutex_init shared_data");
+    
     pthread_mutexattr_destroy(&mutex_attr);
 
-
-    for(int i = 0; i < n; ++i) {
-        shm_ptr[i] = i+1;
+    // 5. Inicjalizacja tablicy produktów, przetasowanie i wypisanie [cite: 26, 27]
+    srand(time(NULL));
+    for (int i = 0; i < n; ++i) {
+        shm_ptr[i] = i + 1;
     }
     shuffle(shm_ptr, n);
+    
+    printf("Initial shop state:\n");
     print_array(shm_ptr, n);
 
-    for(int i = 0;i<m;i++){
+    // 6. Tworzenie procesów pracowników [cite: 28]
+    for (int i = 0; i < m; i++) {
         pid_t pid = fork();
-        if(pid == -1) ERR("fork)");
-        if(pid == 0){
-            children_work(i,shm_ptr,mutex_ptr,n);
+        if (pid == -1) ERR("fork worker");
+        if (pid == 0) {
+            children_work(i, shm_ptr, mutex_ptr, n, m);
             exit(EXIT_SUCCESS);
         }
     }
+
+    // 7. Tworzenie procesu kierownika [cite: 37]
+    pid_t manager_pid = fork();
+    if (manager_pid == -1) ERR("fork manager");
+    if (manager_pid == 0) {
+        manager_work(shm_ptr, mutex_ptr, n, m);
+        exit(EXIT_SUCCESS);
+    }
+
+    // 8. Oczekiwanie na zakończenie wszystkich procesów 
+    // Czekamy na M pracowników + 1 kierownika
+    for (int i = 0; i < m + 1; i++) {
+        wait(NULL);
+    }
+
+    // 9. Komunikat końcowy i sprzątanie 
+    printf("Night shift in Bitronka is over\n");
+    print_array(shm_ptr, n);
+
+    // Niszczenie muteksów i odmapowanie pamięci
+    for (int i = 0; i < n; i++) pthread_mutex_destroy(&mutex_ptr[i]);
+    pthread_mutex_destroy(&sd->mutex);
+    
+    munmap(shm_ptr, n * sizeof(int));
+    munmap(shm_base, shared_size);
+
+    return EXIT_SUCCESS;
 }
