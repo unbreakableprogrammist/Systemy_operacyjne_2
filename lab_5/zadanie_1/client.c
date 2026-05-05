f802c70f2ffed007dd0ad3866294ca3aac759bc7
krzysztof <krzysztof.wojczakowski@gmail.com>
2026-05-05 20:02:39 +0200

    all

--- lab_5/zadanie_1/client.c
+++ lab_5/zadanie_1/client.c
@@ -1,77 +1,111 @@
+// Włącza GNU-specyficzne funkcje systemowe
 #define _GNU_SOURCE
+// Dodatkowe nagłówki do obsługi błędów
 #include <errno.h>
+// Nagłówek do obsługi flag otwierania plików (O_WRONLY, O_RDONLY, itp.)
 #include <fcntl.h>
+// Nagłówek z limitami (PIPE_BUF)
 #include <limits.h>
+// Nagłówek do obsługi wejścia/wyjścia
 #include <stdio.h>
+// Nagłówek do obsługi alokacji pamięci (malloc, exit)
 #include <stdlib.h>
+// Nagłówek do obsługi stringów (memset)
 #include <string.h>
+// Nagłówek do operacji na plikach (S_IRUSR, S_IWUSR, itd.)
 #include <sys/stat.h>
+// Nagłówek z typami systemowymi (pid_t)
 #include <sys/types.h>
+// Nagłówek z funkcjami POSIX (read, write, close, unlink)
 #include <unistd.h>
 
+// Definiuje rozmiar wiadomości: cały bufor PIPE_BUF minus miejsce na PID
 #define MSG_SIZE (PIPE_BUF - sizeof(pid_t))
+// Makro do obsługi błędów: wypisuje informację o błędzie, plik i linię, a następnie kończy program
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
 
+// Funkcja wyświetlająca instrukcję użytkowania programu
 void usage(char *name)
 {
+    // Wypisuje komunikat z poprawną składnią uruchomienia
     fprintf(stderr, "USAGE: %s fifo_file file\n", name);
+    // Zakańcza program z kodem błędu
     exit(EXIT_FAILURE);
 }
 
+// Funkcja do wysyłania zawartości pliku do FIFO w paczkach atomowych
 void write_to_fifo(int fifo, int file) {
+    // Licznik bajtów przeczytanych z pliku
     ssize_t count;
-    char buffer[PIPE_BUF]; // bufor na cala wiadomosc (PID + tekst)
+    // Bufor o rozmiarze PIPE_BUF na całą wiadomość (PID + tekst)
+    char buffer[PIPE_BUF];
+    // Wskaźnik do części bufora zawierającej dane (bez PID)
     char *buf;
 
-    // najpierw robumy z buffe
+    // Zapisuje PID bieżącego procesu na początek bufora
     *((pid_t *)buffer) = getpid();
 
-    // Ustawiamy wskaźnik czytania zaraz za zapisanym PID-em
+    // Ustawia wskaźnik buf zaraz za zapisanym PID-em (przesunięcie o rozmiar pid_t)
     buf = buffer + sizeof(pid_t); 
 
+    // Pętla czytająca plik chunk po chunk
     do {
-        count = TEMP_FAILURE_RETRY(read(file, buf, MSG_SIZE)); // czytamy z pliku do bufora
+        // Czyta dane z pliku do bufora (od pozycji buf), z obsługą przerwań sygnałami
+        count = TEMP_FAILURE_RETRY(read(file, buf, MSG_SIZE));
+        // Jeśli read zwrócił -1, oznacza to błąd
         if (count < 0) ERR("read");
         
-        if (count > 0) { // Przetwarzamy i wysyłamy tylko, jeśli wczytaliśmy jakieś dane
+        // Jeśli przeczytaliśmy jakieś dane (count > 0)
+        if (count > 0) {
+            // Jeśli przeczytano mniej niż MSG_SIZE (np. koniec pliku)
             if (count < MSG_SIZE) {
-                // Zabezpieczenie: jeśli wczytano mniej niż MSG_SIZE (np. koniec pliku),
-                // dopełniamy resztę zerami, by zawsze wysłać idealnie PIPE_BUF bajtów.
+                // Dopełnia resztę bufora zerami, by zawsze wysłać dokładnie PIPE_BUF bajtów
                 memset(buf + count, 0, MSG_SIZE - count);
             }
-            // Zawsze wysyłamy całą paczkę PIPE_BUF, dzięki czemu zachowujemy atomowość
+            // Wysyła całą paczkę PIPE_BUF bajtów do FIFO (zawsze atomowo)
             if (write(fifo, buffer, PIPE_BUF) < 0) {
+                // Jeśli write zwrócił -1, oznacza to błąd
                 ERR("Write");
             }
         }
+    // Pętla trwa, dopóki read zwraca > 0 (koniec pliku zwraca 0)
     } while (count > 0);
 }
 
+// Główna funkcja programu klienta
 int main(int argc, char** argv) { 
-    int fifo, file;
+    // Deskryptor pliku dla FIFO
+    int fifo;
+    // Deskryptor pliku dla pliku wejściowego
+    int file;
     
-    // Usunięto cudzysłowy z argv[0]
+    // Sprawdzenie, czy podano dokładnie 3 argumenty (nazwa programu + FIFO + plik)
     if (argc != 3) usage(argv[0]);
 
-    // Otwieramy/tworzymy FIFO obronnie
+    // Próbuje utworzyć FIFO z odpowiednimi uprawnieniami
     if (mkfifo(argv[1], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP) < 0) {
+        // Jeśli błąd nie dotyczy już istniejącego FIFO (EEXIST), wypisz błąd
         if (errno != EEXIST) ERR("fifo create"); 
     }
     
-    // Klient otwiera do pisania
+    // Otwiera FIFO w trybie pisania (do wysłania danych)
     fifo = open(argv[1], O_WRONLY); 
+    // Jeśli otwieranie się nie powiodło
     if (fifo < 0) ERR("open fifo");
     
-    // Klient otwiera plik z danymi do czytania
+    // Otwiera plik podany jako argument do odczytu
     file = open(argv[2], O_RDONLY); 
+    // Jeśli otwieranie się nie powiodło
     if (file < 0) ERR("open file");
 
-    // Rozpoczynamy przesyłanie
+    // Wysyła zawartość pliku do FIFO
     write_to_fifo(fifo, file);
 
-    // Sprzątamy deskryptory (ale nie usuwamy samego pliku FIFO, zostawiamy to serwerowi)
+    // Zamyka deskryptor pliku wejściowego
     if (close(file) < 0) ERR("close file");
+    // Zamyka deskryptor FIFO (ale nie usuwa pliku FIFO - robi to serwer)
     if (close(fifo) < 0) ERR("close fifo");
     
+    // Zwraca sukces
     return EXIT_SUCCESS;
 }
--- lab_5/zadanie_1/serwer.c
+++ lab_5/zadanie_1/serwer.c
@@ -1,63 +1,102 @@
+// Włącza GNU-specyficzne funkcje systemowe
 #define _GNU_SOURCE
+// Nagłówek do obsługi funkcji znaków (isalnum)
 #include <ctype.h>
+// Nagłówek do obsługi błędów (errno)
 #include <errno.h>
+// Nagłówek do obsługi flag otwierania plików (O_RDONLY)
 #include <fcntl.h>
+// Nagłówek z limitami (PIPE_BUF)
 #include <limits.h>
+// Nagłówek do obsługi wejścia/wyjścia (printf)
 #include <stdio.h>
+// Nagłówek do obsługi alokacji pamięci i exit
 #include <stdlib.h>
+// Nagłówek do operacji na plikach (S_IRUSR, S_IWUSR, itd.)
 #include <sys/stat.h>
+// Nagłówek z typami systemowymi (pid_t)
 #include <sys/types.h>
+// Nagłówek z funkcjami POSIX (read, write, close, unlink)
 #include <unistd.h>
 
+// Makro do obsługi błędów: wypisuje informację o błędzie, plik i linię, a następnie kończy program
 #define ERR(source) (perror(source), fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), exit(EXIT_FAILURE))
+
+// Funkcja wyświetlająca instrukcję użytkowania programu
 void usage(char *name)
 {
+    // Wypisuje komunikat z poprawną składnią uruchomienia
     fprintf(stderr, "USAGE: %s fifo_file\n", name);
+    // Kończy program z kodem błędu
     exit(EXIT_FAILURE);
 }
 
+// Funkcja do odczytywania i przetwarzania danych z FIFO
 void read_from_fifo(int fifo)
 {
-    ssize_t count; // zmienna do zapisywania ile wczytamy z fifo
-    char buffer[PIPE_BUF]; // bufor na cala wiadomosc (PID + tekst)
+    // Licznik bajtów przeczytanych z FIFO
+    ssize_t count;
+    // Bufor o rozmiarze PIPE_BUF na całą wiadomość (PID + tekst)
+    char buffer[PIPE_BUF];
 
+    // Pętla czytająca dane z FIFO
     do
     {
-        count = TEMP_FAILURE_RETRY(read(fifo, buffer, PIPE_BUF)); // czytamy sobie cala wiadomosc z fifo
+        // Czyta paczkę PIPE_BUF bajtów z FIFO, z obsługą przerwań sygnałami
+        count = TEMP_FAILURE_RETRY(read(fifo, buffer, PIPE_BUF));
+        // Jeśli read zwrócił -1, oznacza to błąd
         if (count < 0)
-            ERR("read"); // jesli read zwrocil -1 to oznacza ze byl jakis blad
-        // jesli count == 0 to oznacza ze koniec wczytywania
+            ERR("read");
+        // Jeśli count == 0, oznacza koniec transmisji
+        
+        // Jeśli przeczytaliśmy jakieś dane (count > 0)
         if(count>0){ 
+            // Wypisuje PID procesu klienta, który wysłał wiadomość (zapisany na początku bufora)
             printf("\nPID :%d : \n", *((pid_t *)buffer));
+            // Pętla iterująca przez wszystkie bajty wiadomości (od pozycji pid_t do końca)
             for(int i=sizeof(pid_t);i<count;i++){
+                // Jeśli bajt jest znakiem alfanumerycznym, wypisz go
                 if(isalnum(buffer[i])) printf("%c",buffer[i]);
             }
         }
+    // Pętla trwa, dopóki read zwraca > 0
     } while (count > 0);
 }
 
+// Główna funkcja programu serwera
 int main(int argc, char **argv)
 {
-    int fifo; // file descritpor na fifo
+    // Deskryptor pliku dla FIFO
+    int fifo;
+    // Sprawdzenie, czy podano dokładnie 2 argumenty (nazwa programu + nazwa FIFO)
     if (argc != 2)
         usage(argv[0]);
-    // S_IRUSR  -  permission for owner to read
-    // S_IWUSR -  permission for owner to write
-    // S_IRGRP -  permission for group to read
-    // S_IWGRP -  permission for group to write
+    // S_IRUSR  -  uprawnienie dla właściciela do odczytu
+    // S_IWUSR -  uprawnienie dla właściciela do pisania
+    // S_IRGRP -  uprawnienie dla grupy do odczytu
+    // S_IWGRP -  uprawnienie dla grupy do pisania
+    
+    // Próbuje utworzyć FIFO z odpowiednimi uprawnieniami
     if (mkfifo(argv[1], S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP) < 0)
     {
+        // Jeśli błąd nie dotyczy już istniejącego FIFO (EEXIST), wypisz błąd
         if (errno != EEXIST)
-            ERR("mkfifo create"); // jesli fifo nie istnieje to blad tworzenia
+            ERR("mkfifo create");
     }
+    
+    // Otwiera FIFO w trybie odczytu (do odebrania danych)
     if ((fifo = open(argv[1], O_RDONLY)) < 0)
-        ERR("open"); // otwieramy fifo do odczytu
+        ERR("open");
 
-    read_from_fifo(fifo); // funkcja do odczytu danych z fifo
+    // Odczytuje i przetwarza dane z FIFO
+    read_from_fifo(fifo);
 
+    // Zamyka deskryptor FIFO
     if(close(fifo)<0) ERR("close fifo");
 
-    if(unlink(argv[1]) < 0) ERR("unlink"); // usuwamy fifo po zakonczeniu pracy programu
+    // Usuwa plik FIFO z systemu plików po zakończeniu pracy
+    if(unlink(argv[1]) < 0) ERR("unlink");
 
+    // Zwraca sukces
     return EXIT_SUCCESS;
 }
