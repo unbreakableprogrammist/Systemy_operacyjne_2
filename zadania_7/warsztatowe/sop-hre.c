2d495d237265f94503d85a1bca32bf4b87370d2a
krzysztof <krzysztof.wojczakowski@gmail.com>
2026-05-06 11:17:04 +0200

    poczatek warsztatowych

---
+++ zadania_7/warsztatowe/sop-hre.c
@@ -0,0 +1,23 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+void usage(char* name)
+{
+    fprintf(stderr, "USAGE: %s port\n", name);
+    exit(EXIT_FAILURE);
+}
+
+int main() { return 0; }
